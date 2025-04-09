240403fc4403db4885eb047521e4ff0dc7ed30d4
eishin <applejack9972@gmail.com>
2025-04-09 14:50:24 +0800

    feat:Change line_notifier from send TextMessage to FlexMessage

--- line_notifier.py
+++ line_notifier.py
@@ -1,6 +1,6 @@
 import os
 from linebot import LineBotApi #Line Messaging API 互動的核心類別
-from linebot.models import TextMessage
+from linebot.models import TextMessage, FlexSendMessage
 
 # 從 GitHub Actions 的環境變數讀取 Token
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
@@ -8,9 +8,47 @@ channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 # 初始化 LineBotApi 物件
 line_bot_api = LineBotApi(channel_access_token)
 
-def send_text_message(message):
+def send_pm25_flex_message(message):
+    flex_meaasge = FlexSendMessage(
+        alt_text = "PM2.5觀測數據",
+        contents={
+            "type":"bubble",
+            "header":{
+                "type":"box",
+                "layout":"vertical",
+                "contents":[
+                    {
+                        "type":"text",
+                        "text":"最新PM2.5觀測結果",
+                        "weight":"bold",
+                        "size":"xl",
+                        "color":"#000000"
+                    }
+                ]
+            },
+            "body":{
+                "type":"box",
+                "layout":"vertical",
+                "contents":[
+                    {
+                        "type":"text",
+                        "text":""
+                    },
+                    {
+                        "type":"buttom",
+                        "sytle":"primary",
+                        "action":{
+                            "type":"uri",
+                            "label":"查看全台PM2.5觀測結果",
+                            "uri":"https://eishinchen.github.io/aether_bot/pm25_map.html"
+                        }
+                    }
+                ]
+            }
+        }
+    )
     try:
-        line_bot_api.broadcast(TextMessage(text=message))
+        line_bot_api.broadcast(flex_meaasge)
         print("訊息發送成功")
     except Exception as e:
         print("發送失敗:", e)
